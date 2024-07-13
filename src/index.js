8f43e44f22fd8111d0f3c5d7f0332952a70ecad2
Ashutosh Verma <vashutosh625@gmail.com>
2024-07-14 01:21:20 +0530

    configured server and first route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
-import connectDB from './db/index.js'
-import { app } from './app.js';
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 console.log(process.env);
 
-
-connectDB().then(()=>{
-
-}).catch((err)=>{
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
     console.log("mongodb connection failed");
-    
-})
-
-
-
+  });
